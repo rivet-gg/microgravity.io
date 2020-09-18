4efa09816b985fca1d0979e66f5a016e3b715acf
Bobby-McBobface <54757965+Bobby-McBobface@users.noreply.github.com>
2020-09-18 11:28:00 +0800

    added demolish button

--- src/client/GameClient.js
+++ src/client/GameClient.js
@@ -151,6 +151,8 @@ class GameClient extends Game {
 
         /* @type {?number} */ this.deathPosX = null;
         /* @type {?number} */ this.deathPosY = null;
+        
+        /* @type {boolean} */ this.demolishConfirm = false; 
 
         this.recognition = null;
 
@@ -2471,6 +2473,13 @@ class GameClient extends Game {
                     addAction(`structureSharingToggle`, `[${utils.translate(structure.allianceSharing ? "on-label" : "off-label").toUpperCase()}] ${utils.translate("toggle-sharing")}`, () => {
                         this.sendStructureAction(structures.actions.TOGGLE_SHARING, !structure.allianceSharing);
                     });
+                    //Demolish + Confirm messages
+                    if (!this.demolishConfirm) {
+                        addAction("demolish", `${utils.translate("demolish")}`, () => {this.demolishConfirm=true}, true);
+                    }
+                    else if (this.demolishConfirm) {addAction("demolish", `${utils.translate("confirmation-question")}`, () => {
+                        this.sendStructureAction(structures.actions.DEMOLISH, true);
+                    });}       
                 }
             }
 
@@ -2487,6 +2496,7 @@ class GameClient extends Game {
             }
         } else {
             structurePanel.style.display = "none";
+            this.demolishConfirm = false //Make the demolish promt reset if go off structure
         }
     }
 
