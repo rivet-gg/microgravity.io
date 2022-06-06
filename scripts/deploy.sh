7ad56f4fd1fe3ee94461c7969c6d68809b05675d
Nathan Flurry <developer@nathanflurry.com>
2022-06-06 15:41:57 -0700

    Trying to fix build-prod

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,7 +18,7 @@ docker build --file Dockerfile --tag microgravity --squash .
 rivetctl-cli build push microgravity --name "Local"
 
 rm -f public/*.js public/*.js.map
-npm run build-prod
+# npm run build-prod
 npm run build
 rivetctl-cli site push ./public/ --name "Local"
 
