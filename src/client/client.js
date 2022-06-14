208e18d1c59f86e6876b02064a8820c2dd54c09e
MasterPtato <maxpopkov.contact@gmail.com>
2022-06-14 12:46:59 -0700

    Add hcaptcha

--- src/client/client.js
+++ src/client/client.js
@@ -17,25 +17,11 @@ function start() {
 		const clientApi = new mm.MatchmakerService({
 			endpoint: ENV_API_MATCHMAKER_URL ?? 'https://matchmaker.api.rivet.gg/v1',
 			tls: true,
+			maxAttempts: 0,
 			requestHandler: api.requestHandlerMiddleware()
 		});
 
-		clientApi
-			.findLobby({
-				gameModes: ['default'],
-				preventAutoCreateLobby: false
-			})
-			.then(res => {
-				console.log('Found lobby', res);
-				if (!res.lobby) throw 'Missing lobby';
-				let port = res.lobby.ports['default'];
-				game.connectSocket(port.hostname, port.port, port.isTls, 0, res.lobby.player.token);
-			})
-			.catch(err => {
-				console.error('Failed to find lobby:', err);
-				alert('Failed to find lobby:' + JSON.stringify(err));
-				return;
-			});
+		connectToLobby(clientApi);
 	} else {
 		// Create dev socket
 		game.connectSocket('127.0.0.1', 8008, false, 0);
@@ -43,3 +29,42 @@ function start() {
 }
 
 window.addEventListener('load', start);
+
+function connectToLobby(clientApi, captcha) {
+	document.querySelector('#hCaptcha').style.display = 'none';
+
+	clientApi
+		.findLobby({
+			gameModes: ['default'],
+			preventAutoCreateLobby: false,
+			captcha
+		})
+		.then(res => {
+			console.log('Found lobby', res);
+			if (!res.lobby) throw 'Missing lobby';
+			let port = res.lobby.ports['default'];
+			game.connectSocket(port.hostname, port.port, port.isTls, 0, res.lobby.player.token);
+		})
+		.catch(err => {
+			// Request captcha on error
+			if (err.code == 'CAPTCHA_REQUIRED') {
+				document.querySelector('#hCaptcha').style.display = 'flex';
+
+				hcaptcha.render('hCaptcha', {
+					sitekey: err.metadata.hcaptcha.site_id,
+					callback: clientResponse => {
+						connectToLobby(clientApi, {
+							hcaptcha: {
+								clientResponse
+							}
+						});
+					}
+				});
+			} else {
+				console.error('Failed to find lobby:', err);
+
+				delete err.$metadata;
+				alert('Failed to find lobby:' + JSON.stringify(err));
+			}
+		});
+}
