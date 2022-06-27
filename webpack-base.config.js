07549a3e9829ad2e45ab6055613ba3391cb66c5f
Nathan Flurry <developer@nathanflurry.com>
2022-06-27 18:15:04 +0000

    Split webpack files

---
+++ webpack-base.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let defineValues = {};
+
+// Expose environment variables
+const envValues = {
+	RIVET_PARTY_API_URL: process.env.RIVET_PARTY_API_URL,
+	RIVET_IDENTITY_API_URL: process.env.RIVET_IDENTITY_API_URL,
+	RIVET_MATCHMAKER_API_URL: process.env.RIVET_MATCHMAKER_API_URL,
+	RIVET_CLIENT_TOKEN: process.env.RIVET_CLIENT_TOKEN,
+};
+console.log('envValues:', envValues);
+
+module.exports = {
+	entry: {
+		client: './src/client/client.js'
+	},
+	devtool: 'source-map',
+	mode: 'development',
+	output: {
+		filename: '[name].[chunkhash:6].js',
+		path: path.join(__dirname, 'public'),
+		publicPath: '/'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /\.html$/,
+				use: {
+					loader: 'html-loader',
+					options: {
+						minimize: true
+					}
+				}
+			}
+		]
+	},
+	devServer: {
+		port: 8080,
+		static: {
+			directory: path.join(__dirname, 'public'),
+		},
+	},
+	plugins: [
+		new webpack.EnvironmentPlugin(envValues),
+		new HtmlWebpackPlugin({
+			// Compile HTML so it matches the client hash
+			template: path.join(__dirname, 'src', 'index.html'),
+			minify: true
+		})
+	],
+	stats: {
+		colors: true
+	},
+	externals: {
+		redis: true
+	}
+};
--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,11 +1,12 @@
-const devConfig = require('./webpack-dev.config.js');
+const baseConfig = require('./webpack-base.config.js');
 
-// Override the env
+require('dotenv').config({ path: ".env.prod" });
+
 process.env['NODE_ENV'] = 'production';
 process.env['BABEL_ENV'] = 'production';
 
-module.exports = Object.assign({}, devConfig, {
+module.exports = Object.assign({}, baseConfig, {
 	// Disabled since this breaks something
 	// devtool: "nosources-source-map",  // This will replace any older source map
-	// mode: "production"
+	mode: "production"
 });
