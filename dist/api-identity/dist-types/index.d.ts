f6214678a424a2bceeedea58bf5621ef6c1b7804
Nathan Flurry <developer@nathanflurry.com>
2022-10-05 02:02:24 +0000

    Remove dist

--- dist/api-identity/dist-types/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from "./IdentityServiceClient";
-export * from "./IdentityService";
-export * from "./commands";
-export * from "./models";
-export * as common from "@rivet-gg/common";
-export { IdentityServiceServiceException } from "./models/IdentityServiceServiceException";
-export * from './IdentityManager';
