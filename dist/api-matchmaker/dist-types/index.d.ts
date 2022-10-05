518de8312f11035116818aa79b64bf409d1ed9a4
Nathan Flurry <developer@nathanflurry.com>
2022-10-05 02:02:24 +0000

    Remove dist

--- dist/api-matchmaker/dist-types/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./MatchmakerServiceClient";
-export * from "./MatchmakerService";
-export * from "./commands";
-export * from "./models";
-export * as common from "@rivet-gg/common";
-export { MatchmakerServiceServiceException } from "./models/MatchmakerServiceServiceException";
