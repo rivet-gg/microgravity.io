5dd856dbe2f3e8c3b946ef44a4a4a4d48b40e8ce
Nathan Flurry <developer@nathanflurry.com>
2023-02-10 18:38:43 +0000

    Remove generated libraries

--- gen/api-matchmaker/clients/client/src/index.ts
+++
@@ -1,7 +0,0 @@
-// smithy-typescript generated code
-export * from "./MatchmakerServiceClient";
-export * from "./MatchmakerService";
-export * from "./commands";
-export * from "./models";
-export * as common from "@rivet-gg/common";
-export { MatchmakerServiceServiceException } from "./models/MatchmakerServiceServiceException";
--- gen/api-party/clients/client/src/index.ts
+++
@@ -1,7 +0,0 @@
-// smithy-typescript generated code
-export * from "./PartyServiceClient";
-export * from "./PartyService";
-export * from "./commands";
-export * from "./models";
-export * as common from "@rivet-gg/common";
-export { PartyServiceServiceException } from "./models/PartyServiceServiceException";
